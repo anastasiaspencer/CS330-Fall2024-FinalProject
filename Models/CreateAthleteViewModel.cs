6b4d23041780f6ed38beb83081c7963960c57c40
anastasiaspencer <spenceranastasia7@gmail.com>
2024-11-26 01:17:01 -0500

    making create athlete view for admin/coach

---
+++ Models/CreateAthleteViewModel.cs
@@ -0,0 +1,10 @@
+public class CreateAthleteViewModel
+{
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public double BestTime { get; set; }
+    public double TopSpeed { get; set; }
+    public double BestDistance { get; set; }
+    public double VerticalDrop { get; set; }
+    public double Ranking { get; set; }
+}
